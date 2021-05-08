c2e1f37e1a01b9399ffeb05f5ce57ffc7077ab29
nayyyhaa <nehaguptang.ng@gmail.com>
2021-05-08 20:52:32 +0530

    feat: hamburger

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+/*front page parallax*/
 let star = document.querySelector('#star');
 let moon = document.querySelector('#moon');
 let behind = document.querySelector('#behind');
@@ -5,6 +6,12 @@ let front = document.querySelector('#front');
 let homeText = document.querySelector('#home-text');
 let homeText2 = document.querySelector('#home-text2');
 
+/*nav bar*/
+let hamburgerBars = document.querySelector('.fa');
+let navItems = document.querySelector(".nav-items");
+let links = navItems.querySelectorAll("a");
+
+/*parallax*/
 document.addEventListener('scroll', function(){
     let value = window.scrollY;
     star.style.left = value + 'px';
@@ -12,4 +19,22 @@ document.addEventListener('scroll', function(){
     behind.style.top = value*0.5 + 'px';
     front.style.top = value*0 + 'px';
     homeText.style.marginTop= value*1.5 + 'px';
+})
+
+/*Hamburger*/
+hamburgerBars.addEventListener("click", () => {
+    navItems.classList.toggle("show");
+    if(hamburgerBars.className == "fa fa-bars")
+        hamburgerBars.className = "fa fa-times";
+    else
+        hamburgerBars.className = "fa fa-bars";
+})
+links.forEach(link => {
+    link.addEventListener("click", () =>{
+        navItems.classList.toggle("show");
+        if(hamburgerBars.className == "fa fa-bars")
+        hamburgerBars.className = "fa fa-times";
+    else
+        hamburgerBars.className = "fa fa-bars";
+    })
 })
